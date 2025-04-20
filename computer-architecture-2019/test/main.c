6067bff4aff350a6a855988d8509c062b6a6f10a
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-04-20 14:23:16 +0900

    スタックメモリのオーバーフロー修正

--- computer-architecture-2019/test/main.c
+++ computer-architecture-2019/test/main.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include <time.h>
 
-#define M 4
-#define N 4
+#define SIZE 100
+
+double A[SIZE][SIZE];
+double B[SIZE][SIZE];
+double C[SIZE][SIZE];
 
 int main() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            A[i][j] = 1.0;
+            B[i][j] = 1.0;
+            C[i][j] = 0.0;
+        }
+    }
+
     const clock_t start = clock();
 
-    double A[M][N] = {
-        {1, 0, 3.14, 2.72},
-        {2.72, 1, 0, 3.14},
-        {1, 1, 1, 1},
-        {1, 2, 3, 4}
-    };
-    double B[M][N] = {
-        {1, 1, 0, 3.14},
-        {0, 1, 3.14, 2.72},
-        {0, 1, 1, 0},
-        {4, 3, 2, 1}
-    };
-    double C[N][N] = {};
-
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+    for (int i = 0; i < SIZE; i++) {
+        for (int k = 0; k < SIZE; k++) {
+            const double a = A[i][k];
+            for (int j = 0; j < SIZE; j++) {
+                C[i][j] += a * B[k][j];
             }
         }
     }
 
     const clock_t end = clock();
     const double time = (double) (end - start) / CLOCKS_PER_SEC;
-    printf("time: %lf[ms]\n", time);
+    printf("time: %lf[ms]\n", time * 1000.0);
 
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("C[%d][%d] = %lf\n", i, j, C[i][j]);
-        }
-    }
-
+    // for (int i = 0; i < SIZE; i++) {
+    //     for (int j = 0; j < SIZE; j++) {
+    //         printf("C[%d][%d] = %lf\n", i, j, C[i][j]);
+    //     }
+    // }
 }
