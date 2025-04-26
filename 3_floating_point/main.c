f355f318be26ced771ae12422684f4fce7384b72
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-04-26 16:32:13 +0900

    doubleの最小の浮動小数点数を求めるプログラムを実装

---
+++ 3_floating_point/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double x = 1.0;
+    int count = 0;
+
+    while (x > 0.0) {
+        printf("%d, x: %.20e\n", count, x);
+        x /= 2.0;
+        count++;
+    }
+
+    printf("count: %d\n", count);
+}
