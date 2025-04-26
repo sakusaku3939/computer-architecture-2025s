61604cccc7601d4df6b10266a85280f379c6ba52
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-04-26 16:34:41 +0900

    float版を実装

--- 3_floating_point/main.c
+++ 3_floating_point/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    double x = 1.0;
+    float x = 1.0f;
     int count = 0;
 
-    while (x > 0.0) {
+    while (x > 0.0f) {
         printf("%d, x: %.20e\n", count, x);
-        x /= 2.0;
+        x /= 2.0f;
         count++;
     }
 
