7de5513ba8ed290340ce6f548a1927493f6fb52f
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-05-04 21:56:49 +0900

    arm行列乗算の実装

--- 5_arm/matrix.s
+++ 5_arm/matrix.s
@@ -1,113 +1,65 @@
+.code 32
+bl _start
+swi 6
+
+.global _start
+_start:
+    push    {lr}
+    mov     r14, #3
+    ldr     r1, =A
+    ldr     r2, =B
+    ldr     r3, =C
+    mov     r4, #0
+loop_i:
+    cmp     r4, #3
+    bge     end
+    mov     r5, #0
+loop_j:
+    cmp     r5, #3
+    bge     next_i
+    mov     r6, #0
+    mul     r7, r4, r14
+    mov     r9, #0
+loop_k:
+    cmp     r9, #3
+    bge     store
+    mul     r8, r9, r14
+    add     r8, r8, r5
+    lsl     r8, r8, #2
+    add     r10, r1, r8
+    ldr     r10, [r10]
+    mul     r8, r4, r14
+    add     r8, r8, r9
+    lsl     r8, r8, #2
+    add     r11, r2, r8
+    ldr     r11, [r11]
+    mul     r12, r10, r11
+    add     r6, r6, r12
+    add     r9, r9, #1
+    b       loop_k
+store:
+    add     r8, r7, r5
+    lsl     r8, r8, #2
+    add     r9, r3, r8
+    str     r6, [r9]
+    add     r5, r5, #1
+    b       loop_j
+next_i:
+    add     r4, r4, #1
+    b       loop_i
+end:
+    pop     {pc}
+
+.data
 A:
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
+    .word   1,1,1
+    .word   1,1,1
+    .word   1,1,1
 B:
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
-        .word   0
-        .word   1072693248
+    .word   1,1,1
+    .word   1,1,1
+    .word   1,1,1
 C:
-        .zero   72
-main:
-        sub     sp, sp, #16
-        str     wzr, [sp, 12]
-        b       .L2
-.L7:
-        str     wzr, [sp, 8]
-        b       .L3
-.L6:
-        str     wzr, [sp, 4]
-        b       .L4
-.L5:
-        adrp    x0, C
-        add     x2, x0, :lo12:C
-        ldrsw   x3, [sp, 4]
-        ldrsw   x1, [sp, 12]
-        mov     x0, x1
-        lsl     x0, x0, 1
-        add     x0, x0, x1
-        add     x0, x0, x3
-        ldr     d30, [x2, x0, lsl 3]
-        adrp    x0, A
-        add     x2, x0, :lo12:A
-        ldrsw   x3, [sp, 8]
-        ldrsw   x1, [sp, 12]
-        mov     x0, x1
-        lsl     x0, x0, 1
-        add     x0, x0, x1
-        add     x0, x0, x3
-        ldr     d29, [x2, x0, lsl 3]
-        adrp    x0, B
-        add     x2, x0, :lo12:B
-        ldrsw   x3, [sp, 4]
-        ldrsw   x1, [sp, 8]
-        mov     x0, x1
-        lsl     x0, x0, 1
-        add     x0, x0, x1
-        add     x0, x0, x3
-        ldr     d31, [x2, x0, lsl 3]
-        fmul    d31, d29, d31
-        fadd    d31, d30, d31
-        adrp    x0, C
-        add     x2, x0, :lo12:C
-        ldrsw   x3, [sp, 4]
-        ldrsw   x1, [sp, 12]
-        mov     x0, x1
-        lsl     x0, x0, 1
-        add     x0, x0, x1
-        add     x0, x0, x3
-        str     d31, [x2, x0, lsl 3]
-        ldr     w0, [sp, 4]
-        add     w0, w0, 1
-        str     w0, [sp, 4]
-.L4:
-        ldr     w0, [sp, 4]
-        cmp     w0, 2
-        ble     .L5
-        ldr     w0, [sp, 8]
-        add     w0, w0, 1
-        str     w0, [sp, 8]
-.L3:
-        ldr     w0, [sp, 8]
-        cmp     w0, 2
-        ble     .L6
-        ldr     w0, [sp, 12]
-        add     w0, w0, 1
-        str     w0, [sp, 12]
-.L2:
-        ldr     w0, [sp, 12]
-        cmp     w0, 2
-        ble     .L7
-        mov     w0, 0
-        add     sp, sp, 16
-        ret
+    .word   0,0,0
+    .word   0,0,0
+    .word   0,0,0
